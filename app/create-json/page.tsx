dc27b6779df7b39c0ff47b523daf11e3d9b94bf6
Quide1 <ezequielr.dev@gmail.com>
2024-08-17 20:47:40 -0300

    add new page for test app

--- app/create-json/page.tsx
+++ app/create-json/page.tsx
@@ -1,10 +1,15 @@
+import JsonViewer from "../components/JsonViewer";
+import SectionJson from "../components/SectionJson";
 
 function page() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-gradient-to-bl from-slate-900 via-purple-900 to-slate-900">
-      <h1>Aca se crea el json</h1>
+    <main className="flex min-h-screen flex-col p-4 border-sky-600 bg-[conic-gradient(at_bottom_right,_var(--tw-gradient-stops))] from-blue-700 via-blue-800 to-gray-900">
+      <section className="flex flex-col flex-grow justify-center w-full h-full  gap-2 border-yellow-600 border-2 lg:flex-row ">
+        <SectionJson/>
+       <SectionJson/>
+      </section>
     </main>
-  )
+  );
 }
 
-export default page
+export default page;
