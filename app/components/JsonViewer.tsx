c77dce0fa73b8ca23f7fb919ea463ea98b2b5a9f
Quide1 <ezequielr.dev@gmail.com>
2024-08-17 20:47:07 -0300

    add json viewer component

---
+++ app/components/JsonViewer.tsx
@@ -0,0 +1,25 @@
+import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
+import { coldarkDark as theme } from "react-syntax-highlighter/dist/esm/styles/prism";
+import jsonExample from "@/app/mock/jsonExample.json";
+const jsonExample2 = {
+  name: "martin",
+  edad: "43"
+}
+function JsonViewer() {
+  const codeString = JSON.stringify(jsonExample, null, 2);
+  const newExample = JSON.stringify(jsonExample2, null, 2)
+  return (
+    <SyntaxHighlighter
+      language="json"
+      showLineNumbers={true}
+      style={theme}
+      customStyle={{
+        backgroundColor: "transparent"
+      }}
+    >
+      {codeString}
+    </SyntaxHighlighter>
+  );
+}
+
+export default JsonViewer;
