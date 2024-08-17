6e7596652adf56878ce56f8e1be804b779f710ce
Quide1 <ezequielr.dev@gmail.com>
2024-08-16 21:18:30 -0300

    create new page

---
+++ app/create-json/page.tsx
@@ -0,0 +1,10 @@
+
+function page() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-gradient-to-bl from-slate-900 via-purple-900 to-slate-900">
+      <h1>Aca se crea el json</h1>
+    </main>
+  )
+}
+
+export default page
